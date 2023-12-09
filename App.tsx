13d084f79f657742c73abc4d0cf60b57f69023d1
Marshall Wang <wmhayt228@gmail.com>
2023-12-09 20:12:29 +0800

    feat(ui): 新增UI自定义主题

--- App.tsx
+++ App.tsx
@@ -2,19 +2,23 @@
  * @Author: Marshall
  * @Date: 2023-12-09 12:37:44
  * @LastEditors: Marshall
- * @LastEditTime: 2023-12-09 19:35:25
+ * @LastEditTime: 2023-12-09 19:49:02
  * @Description:
  * @FilePath: /wuxian-rn-template/App.tsx
  */
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Home from './src/modules/home/Home';
+import {Provider} from '@ant-design/react-native';
+import custom from './src/theme/custom';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <Home />
-    </SafeAreaView>
+    <Provider theme={custom}>
+      <SafeAreaView>
+        <Home />
+      </SafeAreaView>
+    </Provider>
   );
 }
 
---
+++ src/theme/custom.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: Marshall
+ * @Date: 2023-12-09 19:48:20
+ * @LastEditors: Marshall
+ * @LastEditTime: 2023-12-09 19:50:45
+ * @Description:
+ * @FilePath: /wuxian-rn-template/src/theme/custom.tsx
+ */
+// tips：主题修改后需重启应用
+export default {
+  // button_font_size: 32,
+};
